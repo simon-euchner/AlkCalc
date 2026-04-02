0aec34f5eb09d49dfadad2bf8e2a8939d2bbd148
Simon-Euchner <euchner.se@gmail.com>
2026-04-02 21:49:49 +0200

    update/state-parser: Updated parser successfully

--- src.d/alkcalc.c
+++ src.d/alkcalc.c
@@ -6,8 +6,6 @@
 
 #include "../interface.d/alkcalc.h"
 
-#include <time.h> // REMOVEME FIXME !!!
-
 #define CONVERT(X) (int32_t)floor(2.*(X)+.5)
 #define INTEGER_ABS(X) (((X) > 0) ? (X): -1*(X))
 #define MAX(X, Y) ((X) > (Y)) ? (X): (Y)
@@ -15,6 +13,7 @@
 #define COMPLEX(X, Y) ((X)+(Y)*I)
 
 static void move(FILE *, int32_t);
+static inline double parse(const char *, int32_t);
 static alkcalc_cg w3jm(int32_t, int32_t, int32_t, int32_t, int32_t, int32_t);
 static int64_t s64imul(int64_t, int64_t);
 static int64_t ns64imul(int32_t, int64_t *);
@@ -25,9 +24,6 @@ static double complex Ylml(int32_t, int32_t, double, double);
 static double cgtofloat(alkcalc_cg);
 static double thermal_photon_occupation(double, double);
 static void nextrm(char *, int32_t *, int32_t *, int32_t, double);
-
-/* FIXME !!! PUT AT CORRECT POSITION */
-static inline double parse(const char *, int32_t);
 
 /* -------------------------------------------------------------------------- *
  * Eigenenergy in units of Hartree (27.211386245981(30) eV Ref. [5])          *
@@ -92,8 +88,8 @@ double alkcalc_Enlsj(char *species, int32_t n, int32_t l, double j) {
 alkcalc_state *alkcalc_fnlsj(char result, char *species, int32_t n, int32_t l,
                              double j) {
 
-    char file[LEN_PATH_TO_STATES+101], filename[101];
-    int32_t J, N, dim, dummy, k;
+    char file[LEN_PATH_TO_STATES+101], filename[101], *buffer, *bfr;
+    int32_t J, N, dim, dummy, ndi, ndf, a, b, c, d, k;
     double *t, *h, *fnlsj;
     FILE *fd;
     alkcalc_state *state;
@@ -113,7 +109,7 @@ alkcalc_state *alkcalc_fnlsj(char result, char *species, int32_t n, int32_t l,
     (void)fscanf(fd, "NUMBER OF DISCRETISATION POINTS: %d\n", &N);
     move(fd, 2);
 
-    /* Allocate memory */
+    /* Allocate memory for result */
     state = (alkcalc_state *)malloc(sizeof(alkcalc_state));
     state->dim = dim = (state->N = N)-2;
     switch (result) {
@@ -132,25 +128,21 @@ alkcalc_state *alkcalc_fnlsj(char result, char *species, int32_t n, int32_t l,
     }
     state->fnlsj = fnlsj = (double *)malloc(dim*sizeof(double));
 
-    clock_t t0, t1;
-    /* --- NEW ------------------------------------------------------------- */
-    char *buffer = (char *)malloc(dim*22-1); /* Decide if we put sizeof(char) */
-    t0 = clock();
-    (void)fread(buffer, 1, dim*22-1, fd);
-    for (k=0; k<dim; k++) state->fnlsj[k] = parse(buffer+22*k, 15);
-    t1 = clock();
-    free(buffer); buffer = NULL;
-    /* --- NEW ------------------------------------------------------------- */
-    printf("%1.14E ms\n", (double)(t1-t0)/CLOCKS_PER_SEC*1000);
+    /* Number of digits                                                       *
+     *                                                                        *
+     * The number of digits used per integer (ndi) and floating-point value   *
+     * (ndf) in the data files 'states-...' and 'discretisation-...'. For     *
+     * instance, if the floating-point values are of the form '+1.23E+45',    *
+     * ndf=3. If the integers, numbering the discretisation points, are of    *
+     * the form '0123', ndi=4. The integers a, b, c, and d repeatedly appear  *
+     * in the code. They only dependent on ndi and ndf.                       */
+    ndi = 8; ndf = 15;
+    a = ndf+7; b = dim*a-1; c = ndi+1; d = (N-1)*(ndi+1+2*a)-1;
 
-    //clock_t t0, t1;
-    //t0 = clock();
-    ///* --- OLD ------------------------------------------------------------- */
-    ///* Read in components of radial eigenstate in basis of elements */
-    //for (k=0; k<dim; (void)fscanf(fd, "%lf\n", fnlsj+k++));
-    ///* --- OLD ------------------------------------------------------------- */
-    //t1 = clock();
-    //printf("%1.14E ms\n", (double)(t1-t0)/CLOCKS_PER_SEC*1000);
+    /* Read in radial eigenstate */
+    (void)fread(buffer = (char *)malloc(b), 1, b, fd);
+    for (k=0; k<dim; k++) state->fnlsj[k] = parse(buffer+a*k, ndf);
+    free(buffer); buffer = NULL;
 
     /* Close file */
     fclose(fd); fd = NULL;
@@ -170,26 +162,15 @@ alkcalc_state *alkcalc_fnlsj(char result, char *species, int32_t n, int32_t l,
     }
     move(fd, 8);
     (void)fscanf(fd, "%d %lf\n", &dummy, t);
-
-    /* --- NEW ------------------------------------------------------------- */
-    buffer = (char *)malloc((N-1)*53-1);
-    char *bfr = buffer;
-    //if ((N-1)*53-1 != fread(buffer, 1, (N-1)*53-1, fd)) exit(1);
-    (void)fread(buffer, 1, (N-1)*53-1, fd);
-    for (k=0; k<N-1; k++) {
-        bfr += 9;
-        state->t[k+1] = parse(bfr, 15);
-        bfr += 22;
-        state->h[k] = parse(bfr, 15);
-        //printf("%lf\n", state->h[k]);
-        bfr += 22; /* Shifts outside of range: final \n not there FIXME!!!*/
+    (void)fread(bfr = buffer = (char *)malloc(d), 1, d, fd);
+    for (k=0; k<N-2; k++) {
+        state->t[k+1] = parse(bfr += c, ndf);
+        state->h[k] = parse(bfr += a, ndf);
+        bfr += a;
     }
+    state->t[N-2+1] = parse(bfr += c, ndf);
+    state->h[N-2] = parse(bfr += a, ndf);
     free(buffer); buffer = NULL;
-    /* --- NEW ------------------------------------------------------------- */
-
-    ///* --- OLD ------------------------------------------------------------- */
-    //for (k=0; k<N-1; k++) (void)fscanf(fd, "%d %lf %lf\n", &dummy, t+k+1, h+k);
-    ///* --- OLD ------------------------------------------------------------- */
 
     /* Close file */
     fclose(fd); fd = NULL;
@@ -672,6 +653,38 @@ static void move(FILE *fd, int32_t nlines) {
     }
 }
 
+/* Optimised parser for reading data files quickly                            */
+static inline double parse(const char *str, int32_t nd) {
+
+    int8_t s, i;
+    int32_t k;
+    uint64_t p10, dec;
+    double r;
+
+    /* Get sign */
+    s = (str[0] == '-') ? -1: 1;
+
+    /* Leading integer */
+    i = str[1]-'0';
+
+    /* Get decimal places */
+    p10 = 1; dec = str[3]-'0'; /* First digit */
+    for (k=4; k<nd+2; k++) { p10 *= 10; dec = 10*dec + str[k]-'0'; }
+
+    /* Result without power */
+    r = s*(i+(double)dec*.1/p10);
+
+    /* Get exponent */
+    s = (str[nd+3] == '-') ? -1: 1;
+    i = s*((str[nd+4]-'0')*10+(str[nd+5]-'0'));
+
+    /* Add power to result */
+    while (i-- > 0) r *= 10.;
+    while (++i < 0) r *= .1;
+
+    return r;
+}
+
 /* Wigner's 3jm symbols (arguments must be TWICE the desired arguments)       */
 static alkcalc_cg w3jm(int32_t j1, int32_t m1, int32_t j2, int32_t m2,
                        int32_t j3, int32_t m3) {
@@ -908,36 +921,4 @@ static void nextrm(char *species, int32_t *nmin, int32_t *nmax, int32_t l,
 
     /* Clean up */
     fclose(fd); fd = NULL;
-}
-
-/* FIXME !!! PUT AT CORRECT POSITION */
-static inline double parse(const char *str, int32_t ndig) {
-
-    int8_t s, i;
-    int32_t k;
-    uint64_t p10, dec;
-    double r;
-
-    /* Get sign */
-    s = (str[0] == '-') ? -1: 1;
-
-    /* Leading integer */
-    i = str[1]-'0';
-
-    /* Get decimal places */
-    p10 = 1; dec = str[3]-'0'; /* First digit */
-    for (k=4; k<ndig+2; k++) { p10 *= 10; dec = 10*dec + str[k]-'0'; }
-
-    /* Result without power */
-    r = s*(i+(double)dec*.1/p10);
-
-    /* Get exponent */
-    s = (str[ndig+3] == '-') ? -1: 1;
-    i = s*((str[ndig+4]-'0')*10+(str[ndig+5]-'0'));
-
-    /* Add power to result */
-    while (i-- > 0) r *= 10.;
-    while (++i < 0) r *= .1;
-
-    return r;
 }
